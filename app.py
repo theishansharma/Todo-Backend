6366ea69120288603895a454d3aaf85d7b853875
Ishan Sharma <75128029+theishansharma@users.noreply.github.com>
2022-04-07 13:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Connecting the Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qpzciilziucfew:dcd128d48107c7e0f2d4a869e53dca7ccbaeea3b6db800b5c8fdbed2f1136bdb@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d4v4m2vpnvi7u2
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qpzciilziucfew:dcd128d48107c7e0f2d4a869e53dca7ccbaeea3b6db800b5c8fdbed2f1136bdb@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d4v4m2vpnvi7u2
 '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False         
 db = SQLAlchemy(app)
